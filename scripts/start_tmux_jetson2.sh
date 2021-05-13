cd284976da3d260b9a7758f8853d17a5610955bb
Roland Arsenault <roland@ccom.unh.edu>
2021-05-13 09:51:37 -0400

    Split yolo workload between jetsons

---
+++ scripts/start_tmux_jetson2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+LOG_FILE=/home/field/project11/log/start_tmux.txt
+
+echo "" >> ${LOG_FILE}
+echo "#############################################" >> ${LOG_FILE}
+echo "Running start_tmux_jetson2.bash" >> ${LOG_FILE}
+echo $(date) >> ${LOG_FILE}
+echo "#############################################" >> ${LOG_FILE}
+echo "" >> ${LOG_FILE}
+echo "Logs:" >> ${LOG_FILE}
+
+set -e
+
+{
+source /opt/ros/melodic/setup.bash
+source /home/field/project11/catkin_ws/devel/setup.bash
+
+export ROS_WORKSPACE=/home/field/project11/catkin_ws
+export ROS_IP=192.168.100.113
+export ROS_MASTER_URI=http://192.168.100.112:11311
+
+} &>> ${LOG_FILE}
+
+set -v
+
+{
+tmux new -d -s project11 rosrun rosmon rosmon --name=rosmon_ben_jetson2 ben_hardware jetson2.launch
+
+} &>> ${LOG_FILE}
